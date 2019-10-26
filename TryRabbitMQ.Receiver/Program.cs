0ce20a1f0c77a5ac6347d09b69cd3d7e411c5f47
berdankoca <berdankoca@gmail.com>
2019-10-26 18:49:41 +0300

    Added rabbitmq project

---
+++ TryRabbitMQ.Receiver/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace TryRabbitMQ.Receiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Receiver started!");
+
+            var factory = new ConnectionFactory() { HostName = "localhost", Port = 5672 };
+            var conenction = factory.CreateConnection();
+            var channel = conenction.CreateModel();
+            channel.QueueDeclare(queue: "first-queue"
+                , durable: true
+                , exclusive: false
+                , autoDelete: false
+                , arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var message = System.Text.Encoding.UTF8.GetString(ea.Body);
+                Console.WriteLine("Receiver: {0}", message);
+
+                channel.BasicAck(ea.DeliveryTag, false);
+            };
+
+            channel.BasicConsume(queue: "first-queue"
+                , autoAck: false
+                , consumer: consumer);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TryRabbitMQ.Sender/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using RabbitMQ.Client;
+
+namespace TryRabbitMQ.Sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sender started!");
+
+            var factory = new ConnectionFactory() { HostName = "localhost", Port = 5672 };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "first-queue"
+                        , durable: true
+                        , exclusive: false
+                        , autoDelete: false
+                        , arguments: null);
+
+                    for (int i = 0; i < 10000; i++)
+                    {
+                        var message = $"Hello { i }";
+
+                        //There is a few high reliability problem in here
+                        //How can I sure the message really publish in the broker?
+                        //Then how can I manage transaction operation and acknowledge problem?
+                        var properties = channel.CreateBasicProperties();
+                        properties.Persistent = true;
+                        channel.BasicPublish(exchange: ""
+                            , routingKey: "first-queue"
+                            , basicProperties: properties
+                            , body: System.Text.Encoding.UTF8.GetBytes(message));
+
+                        Console.WriteLine("Sender: {0}", message);
+                    }
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
